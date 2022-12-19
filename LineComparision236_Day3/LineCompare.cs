323163348ce29e00b7c109eb346904c5a100ccc3
Surajk37 <surajkhot9993@gmail.com>
2022-12-19 13:33:43 +0530

    Welcome Message Added on Master

---
+++ LineComparision236_Day3/LineCompare.cs
@@ -0,0 +1,13 @@
+﻿namespace LineComparision
+{
+    class LineCompare
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\tWelcome To Line Comparision Computation Program");
+            Console.ReadLine();
+
+        }
+    }
+}
