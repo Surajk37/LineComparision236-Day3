bde1757e8e5fd85e2a05209293948643bf00bf14
Surajk37 <surajkhot9993@gmail.com>
2022-12-19 14:37:23 +0530

    [Suraj] Calculating Length of Line By using Distance Formula

--- LineComparision236_Day3/LineCompare.cs
+++ LineComparision236_Day3/LineCompare.cs
@@ -4,10 +4,18 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\tWelcome To Line Comparision Computation Program\n");
+            Console.WriteLine("Enter a x1 and y1 co-ordinates");
+            int x1=Convert.ToInt32(Console.ReadLine());
+            int y1=Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("\tWelcome To Line Comparision Computation Program");
+            Console.WriteLine("\nEnter a x2 and Y2 co-ordinates");
+            int x2=Convert.ToInt32(Console.ReadLine());
+            int y2=Convert.ToInt32(Console.ReadLine());
+
+            double lengthofLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)); // Distance Formula
+            Console.WriteLine("\nLength of Line is : {0}",lengthofLine);
             Console.ReadLine();
-
         }
     }
 }
